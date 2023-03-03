f6c1b9e12a280fde984da8d164dea7621b04c55e
Marcella Montagnese <marcella.montagnese@gmail.com>
2023-03-03 14:07:03 +0000

    Update qmin scripts
    Align naming of csv and functions to ADNI

--- helper_functions.py
+++ helper_functions.py
@@ -37,8 +37,8 @@ def load_qmin(root_dir, csv):
     subjects = [file.split('/')[8] for file in image_files_list] # for QMIN
 
     # Filtering and sorting the labels from the clinical data csv
-    labels_csv = "/home/mm2075/rds/hpc-work/PASSIAN/QMINDatabase/ClinicalData/qmin_participants.csv"
-    labels_df = pd.read_csv(labels_csv, low_memory=False)[['participant_id','sex','age_bl', 'diagnosis']]   # add a new file ID columns
+    labels_csv = "/home/mm2075/rds/hpc-work/PASSIAN/QMINCaps/qmin_participants.csv"
+    labels_df = pd.read_csv(labels_csv, low_memory=False)[['FID','DX','sex','age_bl']]   # adjusted the names as in ADNI's file
 # Define the new class names and the corresponding dictionary
     new_class_names = {'Vascular dementia': 'Dementia',
                    'Mixed dementia (Alzheimer\'s disease and vascular dementia)': 'Dementia',
@@ -76,20 +76,20 @@ def load_qmin(root_dir, csv):
 		   'Uncertain' : 'Non-degenerative'}
 
     # Replace the old labels with new ones
-    diagnosis_dict = {'diagnosis': new_class_names}
+    diagnosis_dict = {'DX': new_class_names}
     labels_df = labels_df.replace(diagnosis_dict)
-    print(labels_df['diagnosis'].value_counts())
+    print(labels_df['DX'].value_counts())
 
     # Remove rows with diagnosis values currently not in use
     valid_diagnoses = ['CN', 'MCI', 'Dementia']
-    mask = labels_df['diagnosis'].isin(valid_diagnoses)
+    mask = labels_df['DX'].isin(valid_diagnoses)
     labels_df = labels_df[mask]
     # reset the index for the new df
     labels_df = labels_df.reset_index(drop=True)
 
-    print('Class balance on entire dataset (train, validation, test):\n', labels_df['diagnosis'].value_counts())
+    print('Class balance on entire dataset (train, validation, test):\n', labels_df['DX'].value_counts())
     no_labels = [x for x in subjects if
-                 x not in labels_df['participant_id'].values]  # these subjects are missing labels (delete from dataset)
+                 x not in labels_df['FID'].values]  # these subjects are missing labels (delete from dataset)
     if no_labels:
         print('The following subjects in the image_files_list are missing labels:\n', no_labels)
         print('Aborting...')
@@ -98,12 +98,12 @@ def load_qmin(root_dir, csv):
     # Convert the new labels to numerical values
     cn = ['CN','MCI', 'Dementia']  # class names
     cd = {cn[0]: 0.0, cn[1]: 1.0, cn[2]: 2.0}  # class dictionary
-    labels_df = labels_df.replace({'diagnosis': cd})
+    labels_df = labels_df.replace({'DX': cd})
     print('Final labels used:\n',
-    labels_df['diagnosis'].value_counts())
+    labels_df['DX'].value_counts())
 
     # Convert the labels to a PyTorch tensor
-    image_class = torch.tensor(list(labels_df['diagnosis']))
+    image_class = torch.tensor(list(labels_df['DX']))
     image_class = image_class.type(torch.LongTensor)
 
     return image_files_list, image_class, cn
